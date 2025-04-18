b403d4f7f6e9e6728c629f4fa128b39b3c651a2b
karl <kralcober@up.edu.ph>
2025-04-18 21:34:40 +0800

    update

--- Life, Love, and Lawbreaking/js/script.js
+++ Life, Love, and Lawbreaking/js/script.js
@@ -1,15 +1,15 @@
+// Smooth scroll
 document.querySelectorAll('a[data-scroll]').forEach(link => {
-    link.addEventListener('click', function (e) {
-      e.preventDefault(); // Stop default jump
-      const targetId = this.getAttribute('href').substring(1);
-      const target = document.getElementById(targetId);
-  
-      if (target) {
-        target.scrollIntoView({
-          behavior: 'smooth',
-          block: 'start'
-        });
-      }
-    });
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const target = document.getElementById(targetId);
+
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
   });
-  
+});
